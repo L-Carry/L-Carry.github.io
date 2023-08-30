98eba1187b6945f812b913f29432891ee469efc6
L-Carrey <aliceGreyrat@protonmail.com>
2023-08-30 18:36:04 +0800

    Site updated: 2023-08-30 18:36:04

--- js/fomal.js
+++ js/fomal.js
@@ -252,18 +252,18 @@ function showWelcome() {
   //根据本地时间切换欢迎语
   let timeChange;
   let date = new Date();
-  if (date.getHours() >= 5 && date.getHours() < 11) timeChange = "<span>上午好</span>，一日之计在于晨！";
+  if (date.getHours() >= 5 && date.getHours() < 11) timeChange = "<span>上午好</span>，早晨起来，拥抱太阳！";
   else if (date.getHours() >= 11 && date.getHours() < 13) timeChange = "<span>中午好</span>，该摸鱼吃午饭了。";
-  else if (date.getHours() >= 13 && date.getHours() < 15) timeChange = "<span>下午好</span>，懒懒地睡个午觉吧！";
-  else if (date.getHours() >= 15 && date.getHours() < 16) timeChange = "<span>三点几啦</span>，一起饮茶呀！";
-  else if (date.getHours() >= 16 && date.getHours() < 19) timeChange = "<span>夕阳无限好！</span>";
-  else if (date.getHours() >= 19 && date.getHours() < 24) timeChange = "<span>晚上好</span>，夜生活嗨起来！";
+  else if (date.getHours() >= 13 && date.getHours() < 15) timeChange = "<span>下午好</span>，中午不睡下午崩溃！";
+  else if (date.getHours() >= 15 && date.getHours() < 16) timeChange = "<span>三点几啦</span>，饮茶先啦！";
+  else if (date.getHours() >= 16 && date.getHours() < 19) timeChange = "<span>要吃晚饭咯！</span>";
+  else if (date.getHours() >= 19 && date.getHours() < 24) timeChange = "<span>夜生活才刚开始！";
   else timeChange = "夜深了，早点休息，少熬夜。";
 
   try {
     //自定义文本和需要放的位置
     document.getElementById("welcome-info").innerHTML =
-      `<b><center>🎉 欢迎信息 🎉</center>&emsp;&emsp;欢迎来自 <span style="color:var(--theme-color)">${pos}</span> 的小伙伴，${timeChange}您现在距离站长约 <span style="color:var(--theme-color)">${dist}</span> 公里，当前的IP地址为： <span style="color:var(--theme-color)">${ip}</span>， ${posdesc}</b>`;
+      `<b><center>🎉 欢迎信息 🎉</center>&emsp;&emsp;欢迎来自 <span style="color:var(--theme-color)">${pos}</span> 的小伙伴，${timeChange}您现在距离站长约 <span style="color:var(--theme-color)">${dist}</span> 公里。`;
   } catch (err) {
     // console.log("Pjax无法获取#welcome-info元素🙄🙄🙄")
   }
